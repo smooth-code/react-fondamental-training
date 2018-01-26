00a2b8f577c1e04722b148504df0d1f0fd17ad9d
Greg Bergé <berge.greg@gmail.com>
2018-01-26 10:25:34 +0100

    [ex-2] variables

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('Hello world!')
+function displayCharacter(name, title) {
+  console.log(name + ' - ' + title)
+}
+
+const characterName = 'Daenerys Targaryen'
+const characterTitle = 'Mother of Dragons'
+
+displayCharacter(characterName, characterTitle)
