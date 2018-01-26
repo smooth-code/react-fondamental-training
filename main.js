3261d6137b673c9b90eec94016733ff1775ca7ec
Greg Bergé <berge.greg@gmail.com>
2018-01-26 11:04:14 +0100

    [ex-5] template strings

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk')
+
 function getElement(name) {
   if (name.match(/stark/i)) return 'ice'
   if (name.match(/targaryen/i)) return 'fire'
@@ -5,8 +7,16 @@ function getElement(name) {
   return null
 }
 
+const elementColors = {
+  ice: 'blue',
+  fire: 'red',
+  'ice and fire': 'magenta',
+}
+
 function displayCharacter({ name, title }) {
-  console.log(name + ' - ' + title + ' - ' + getElement(name))
+  const element = getElement(name)
+  const color = elementColors[element]
+  console.log(chalk`{${color} ${name} - ${title}}`)
 }
 
 const daenerys = { name: 'Daenerys Targaryen', title: 'Mother of Dragons' }
