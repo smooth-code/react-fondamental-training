29c9ce99cbe00d1ed97dac2f9ab94d92bc47ccdc
Greg Bergé <berge.greg@gmail.com>
2018-01-26 16:08:45 +0100

    [ex-7] classes

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
 const chalk = require('chalk')
 
-function getElement(name) {
-  if (name.match(/stark/i)) return 'ice'
-  if (name.match(/targaryen/i)) return 'fire'
-  if (name.match(/snow/i)) return 'ice and fire'
-  return null
-}
+class Character {
+  constructor({ name, title }) {
+    this.name = name
+    this.title = title
+  }
 
-const elementColors = {
-  ice: 'blue',
-  fire: 'red',
-  'ice and fire': 'magenta',
-}
+  getElement() {
+    if (this.name.match(/stark/i)) return 'ice'
+    if (this.name.match(/targaryen/i)) return 'fire'
+    if (this.name.match(/snow/i)) return 'ice and fire'
+    return null
+  }
 
-function displayCharacter({ name, title }) {
-  const element = getElement(name)
-  const color = elementColors[element]
-  console.log(chalk`{${color} ${name} - ${title}}`)
+  getColor() {
+    const elementColors = {
+      ice: 'blue',
+      fire: 'red',
+      'ice and fire': 'magenta',
+    }
+
+    return elementColors[this.getElement()]
+  }
+
+  display() {
+    console.log(chalk`{${this.getColor()} ${this.name} - ${this.title}}`)
+  }
 }
 
 const characters = [
-  { name: 'Daenerys Targaryen', title: 'Mother of Dragons' },
-  { name: 'Aria Stark', title: 'Princess' },
-  { name: 'Jon Snow', title: 'King of the North' },
+  new Character({ name: 'Daenerys Targaryen', title: 'Mother of Dragons' }),
+  new Character({ name: 'Aria Stark', title: 'Princess' }),
+  new Character({ name: 'Jon Snow', title: 'King of the North' }),
 ]
 
-characters.forEach(displayCharacter)
+characters.forEach(function(character) {
+  character.display()
+})
